d48d9ba5b621550fd089beabfa2f8b3c44054f33
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-12-17 17:19:15 -0500

    have two test working for lru_cache.py

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,4 @@
+from doubly_linked_list import DoublyLinkedList
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +8,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.max = limit
+        self.list = DoublyLinkedList()
+        self.storage = dict()
+
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,17 +21,37 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        # looking at the beginning
+        current_item = self.list.head
+        while current_item:
+            if current_item.value == key:
+                #placing our item to the front
+                self.list.move_to_front(current_item)
+            return self.storage[key]
+        return None
 
     """
     Adds the given key-value pair to the cache. The newly-
     added pair should be considered the most-recently used
     entry in the cache. If the cache is already at max capacity
     before this entry is added, then the oldest entry in the
-    cache needs to be removed to make room. Additionally, in the
+    cache seeds to be removed to make room. Additionally, in the
     case that the key already exists in the cache, we simply
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        # first we need to find the key value pair in the list
+        if key in self.storage:
+            self.storage[key] = value
+            self.get(key)
+            return
+        if len(self.list) >= self.max:
+            self.storage.pop(self.list.remove_from_tail())
+
+        self.list.add_to_head(key)
+        self.storage[key] = value
+
+
+
+
