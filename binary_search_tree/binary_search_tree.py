7c6b82b9d0e16626ebfff6195eb5d0db56ff3576
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-12-18 16:57:06 -0500

    The test for contains in test_binary_search_tree.py

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -14,34 +14,47 @@ class BinarySearchTree:
     def insert(self, value):
 
         current_node = self
-        print("current_node", current_node.value)
-        #If inserting we must a already have a tree or root.
+               #If inserting we must a already have a tree or root.
         # If value less than the self.value self.left, make a new tree/node if empty, otherwise
         # keep going (recursion)
-        if current_node.value > value:
-            if current_node.left is None:
-                current_node = BinarySearchTree(value)
-                return
-            current_node = current_node.left
+        while current_node:
+            if current_node.value > value:
+                if current_node.left is None:
+                    current_node.left = BinarySearchTree(value)
+                    return
+                current_node = current_node.left
 
 
 
 
         # if value greater than or equal to then go right, make new tree/node if empty, otherwise
         #keep going
-        elif current_node.value <= value:
-            if current_node.right is None :
-                current_node = BinarySearchTree(value)
-                return
-            current_node = current_node.right
+            elif current_node.value <= value:
+                if current_node.right is None:
+                    current_node.right = BinarySearchTree(value)
+                    return
+                current_node = current_node.right
 
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
+        current_node = self
+
         # if target = self.value, otherwise
         # go left or right
-           pass
+        while current_node:
+            if target == current_node.value:
+                return True
+            elif target > current_node.value:
+                if current_node.right is None:
+                    return False
+                current_node = current_node.right
+            elif target < current_node.value:
+                if current_node.left is None:
+                    return False
+                current_node = current_node.left
+
 
     # Return the maximum value found in the tree
     def get_max(self):
